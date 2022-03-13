d39e68493e229a315661507ba109c828d201c577
L-YuanZhi <70792436+L-YuanZhi@users.noreply.github.com>
2022-03-14 01:41:37 +0900

    Update dataset_pipe.py

--- dataset_pipe.py
+++ dataset_pipe.py
@@ -4,30 +4,36 @@ import cv2
 import os
 import data_normalizer_c as dn 
 
+# SOMの結果に基づいて，パイプ画像を自動で正規化しデータセットを作成プログラム．
+# 正規化はdata_normalizer.pyの関数を使用している．
+# 同時に異なる正規化方法で正規化を行うことができる．
 class dataset_pipe:
-
+    
     def __init__(self,load_path,save_path,flip=True,start=None,stop=None,step=None):
         
-        self.__load_path = load_path
-        self.__save_path = save_path
-        self.__filp = flip
-        self.__angle_list = [0]
+        self.__load_path = load_path # SOMの結果のフォルダ
+        self.__save_path = save_path # 正規化画像を保存するフォルダ
+        self.__filp = flip #　Noneでない場合はミラーリングでデータを水増し
+        self.__angle_list = [0] # 回転で水増しするための角度リスト．
         if start!=None and stop != None and step != None:
-            self.__start = start
-            self.__stop = stop
-            self.__step = step 
-            self.__rotate = True
-            self.__Angle_list_gen()
+            self.__start = start # 回転を開始する角度
+            self.__stop = stop # 回転を止める角度
+            self.__step = step # 回転の間隔
+            self.__rotate = True # trueである場合は回転で水増し
+            self.__Angle_list_gen() # 角度リストを生成
         else:
-            self.__rotate=False
+            self.__rotate=False # 回転しない．
     
     def __Angle_list_gen(self):
+        # 角度リストを生成
         self.__angle_list =[]
         for angle in range(self.__start,self.__stop,self.__step):
             self.__angle_list.append(angle)
 
     def Data_a(self):
-
+    # normalize_circleとmove_averageで正規化を行う．
+    # 正規分布正規化と平均値移動の二つのデータセットを作成する．
+    
         dir_list = os.listdir(self.__load_path)
         for dirs in dir_list:
             label_list = os.listdir(os.path.join(self.__load_path,dirs))
@@ -45,10 +51,10 @@ class dataset_pipe:
                 if label != "lb":
                     for file_name in os.listdir(os.path.join(self.__load_path,dirs,label)):
                         input_image =  plt.imread(os.path.join(self.__load_path,dirs,label,file_name))
-                        norm_image = dn.Normalize_circle(input_image,85,20)
-                        move_image = dn.move_average(input_image,85)
+                        norm_image = dn.Normalize_circle(input_image,85,20)# 正規分布正規化
+                        move_image = dn.move_average(input_image,85)# 平均値移動,mitechと違うのは移動した後に回転する．
                     
-                        for angle in self.__angle_list:
+                        for angle in self.__angle_list:# 水増し
                             rota_image1 = dn.rotation(norm_image,angle)
                             rota_image2 = dn.rotation(move_image,angle)
                             title = file_name+"_"+str(angle)+".bmp"
@@ -60,6 +66,7 @@ class dataset_pipe:
                                 cv2.imwrite(os.path.join(self.__save_path,"move",dirs+"_"+label,title),cv2.flip(rota_image1,1))
 
     def Data_mt(self):
+        # mitechと同じ方法で正規化，回転した後に平均値移動を行う．
         dir_list = os.listdir(self.__load_path)
         for dirs in dir_list:
             label_list = os.listdir(os.path.join(self.__load_path,dirs))
